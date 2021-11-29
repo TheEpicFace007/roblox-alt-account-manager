1c00ba52f5b539dfdcba6e358c36447233726dec
Vini Dalvino <37189791+ViniDalvino@users.noreply.github.com>
2021-11-29 12:23:51 -0500

    Modified popup Added change to make it support different views(alt list, gamelist) Added a filter to the game bar

--- source/Popup/AltItem.tsx
+++ source/Popup/AltItem.tsx
@@ -1,20 +1,26 @@
 import React, { FC } from "react";
+import { BsTrash, BsTrash2Fill, BsTrashFill } from "react-icons/bs"
 
 /**
  * Create an Alt Account Card.
  * It show the profile picture of the alt and it display the alt username
  */
 const AltItem: FC<{
-  username?: string;
-  profilePicture?: string;
-  isCurrent?: boolean;
-  onClick?: (username: string) => void;
-  onDelete?: (usenrame: string) => void;
-}> = ({ username, profilePicture, isCurrent }) => {
+  username: string;
+  profilePicture: string;
+  isCurrent: boolean;
+  onClick: (username: string) => void;
+  onDelete: (usenrame: string) => void;
+}> = ({ username, profilePicture, isCurrent, onClick, onDelete }) => {
+  const [hovered, setHovered] = React.useState(false);
+
   return (
-    <li className={`alt-item${isCurrent ? "-current" : ""}`}>
+    <li className={`alt-item${isCurrent ? "-current" : ""}`} onClick={() => onClick(username)}>
       <h4>{username}</h4>
-      <img src={profilePicture} alt={username} />
+      <div className="alt-item-image-container" >
+        <img src={profilePicture} alt={username} />
+        <BsTrash2Fill size="20px" onClick={() => onDelete(username)}/>
+      </div>
     </li>
   )
 }
@@ -22,8 +28,10 @@ const AltItem: FC<{
 AltItem.displayName = "AltItem";
 AltItem.defaultProps = {
   username: "Roblox",
-  profilePicture: "https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&format=png&username=Roblox",
+  profilePicture: "https://www.roblox.com/Thumbs/Avatar.ashx?x=256&y=256&format=png&username=Roblox",
   isCurrent: false,
+  onClick: () => { },
+  onDelete: () => { }
 }
 
 export default AltItem;
--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -4,21 +4,32 @@ import AltItem from './AltItem';
 import GameGrid from './GameGrid';
 import GameGridItem, { GameCardID } from './GameGridItem';
 import './style.scss'
+import MessageBox from '../shared-component/MessageBox';
+import { iAltAccount } from "../shared/storage"
+
+
+type GamePage = "alt-list" | "game-list";
 
 const Popup: React.FC = () => {
-
+  const [gamePage, setGamePage] = React.useState<GamePage>("game-list");
 
   return (
     <section id="popup">
       <header>
-        <h1>Roblox Alt Manager</h1>
-        <section>
+        <section id="top-bar">
+          <h1>Roblox Alt Manager</h1>
           <button onClick={() => browser.runtime.openOptionsPage()}>Options</button>
-          <button>Manage alts</button>
+          <button
+            style={{ textDecoration: gamePage === "alt-list" ? "underline" : "none" }}
+            onClick={() => {
+              setGamePage(gamePage === "alt-list" ? "game-list" : "alt-list")
+            }}>Manage alts</button>
         </section>
       </header>
       <main>
-        <AltManager />
+        {
+          gamePage === "alt-list" ? <AltManager /> : <AltFavoriteGames />
+        }
       </main>
     </section>
   );
@@ -29,31 +40,50 @@ const Popup: React.FC = () => {
  * This component is used to display an lists of alts and the selcted alts.
  */
 const AltManager: React.FC = () => {
+  const [alts, setAlts] = React.useState<[iAltAccount, boolean][]>([]);
+  const [altToRemove, setAltToRemove] = React.useState<string | null>(null);
+  const [mboxHidden, setMboxHidden] = React.useState(true);
+
+  const onDelPress = (altUsername: string) => {
+    setMboxHidden(false);
+    setAltToRemove(altUsername);
+  }
+
   return (
     <section id="alt-manager">
       <header>
         <h1>Manage alts</h1>
       </header>
       <main>
-        <section>
+        <section id="alt-buttons">
           <button>Add Alt account</button>
-          <input type="text" name="Filter Alts" id="" placeholder="Filter list..." aria-placeholder="Filter-list..."/>
+          <input type="text" name="Filter Alts" id="" placeholder="Filter list..." aria-placeholder="Filter-list..." />
         </section>
         {/* Add a button to quickly add an alt */}
         {/* Display the list of alts */}
-        
+
         <section id="alt-list">
           <ul>
-            <AltItem />
-            <AltItem />
-            <AltItem />
-            <AltItem />
-            <AltItem />
-           <AltItem />
-           <AltItem />
-
+            <AltItem onDelete={onDelPress} />
+            <AltItem onDelete={onDelPress} />
+            <AltItem onDelete={onDelPress} />
+            <AltItem onDelete={onDelPress} />
           </ul>
         </section>
+
+        <MessageBox
+          buttons="yesno"
+          title="Remove alt?"
+          message={`Are you sure you wanna remove ${altToRemove}?`}
+          onClose={(result) => {
+            if (result === "yes") {
+              // Remove the alt from the list
+              console.warn("Not implemented yet")
+            }
+            setMboxHidden(true);
+          }}
+          hidden={mboxHidden} />
+
       </main>
     </section>
   )
@@ -73,20 +103,27 @@ const AltFavoriteGames: React.FC = () => {
   const joinGame = async (_gameId: GameCardID) => {
   }
   return (
-    <GameGrid>
-      {/* Add 8 sample games */}
-      <GameGridItem id={'game-card-item-1'} name="Game 1" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      <GameGridItem id={'game-card-item-2'} name="Game 2" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      <GameGridItem id={'game-card-item-3'} name="Game 3" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      <GameGridItem id={'game-card-item-4'} name="Game 4" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      <GameGridItem id={'game-card-item-5'} name="Game 5" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      <GameGridItem id={'game-card-item-6'} name="Game 6" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      <GameGridItem id={'game-card-item-7'} name="Game 7" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      <GameGridItem id={'game-card-item-8'} name="Game 8" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
-      {/* Add a favorite item */}
-      <GameGridItem id="game-card-item-add-fav" name="Add favorite" onClick={addFavorite} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+    <div className="game-grid">
+      <h1>Favorite games</h1>
+      <section>
+        <button>Add favorite game</button>
+        <input type="text" name="Filter games" id="" placeholder="Filter games..." aria-placeholder="Filter the favorites games" />
+      </section>
 
-    </GameGrid>
+      <div className="game-grid-container">
+        {/* Add 8 sample games */}
+        <GameGridItem id={'game-card-item-1'} name="Game 1" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        <GameGridItem id={'game-card-item-2'} name="Game 2" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        <GameGridItem id={'game-card-item-3'} name="Game 3" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        <GameGridItem id={'game-card-item-4'} name="Game 4" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        <GameGridItem id={'game-card-item-5'} name="Game 5" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        <GameGridItem id={'game-card-item-6'} name="Game 6" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        <GameGridItem id={'game-card-item-7'} name="Game 7" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        <GameGridItem id={'game-card-item-8'} name="Game 8" onClick={joinGame} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+        {/* Add a favorite item */}
+        <GameGridItem id="game-card-item-add-fav" name="Add favorite" onClick={addFavorite} onDelete={deleteFavorite} thumbnail="https://via.placeholder.com/64" />
+      </div>
+    </div>
   )
 }
 
