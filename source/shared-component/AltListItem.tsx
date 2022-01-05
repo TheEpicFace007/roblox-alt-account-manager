b9929649cd28ec2e6714009f684aea3f41724ab4
Vini Dalvino <37189791+ViniDalvino@users.noreply.github.com>
2022-01-05 14:05:04 -0500

    Added feature Added an feature to add an component from an alt username

--- source/shared-component/AltListItem.tsx
+++ source/shared-component/AltListItem.tsx
@@ -1,7 +1,7 @@
-import { Avatar, Button, Col, Container, Grid, Row, Text } from "@nextui-org/react";
-import React from "react";
-import { Play, Star } from "react-iconly";
-import { getRobloxUserHeadshot } from "../shared/roblox";
+import { Avatar, Button, Col, Container, Grid, Row, Spacer, Text } from "@nextui-org/react";
+import React, { useEffect } from "react";
+import { Delete, Play, Star } from "react-iconly";
+import { getRobloxUserById, getRobloxUserHeadshot } from "../shared/roblox";
 
 export type AltList = {
     alt: string,
@@ -19,6 +19,29 @@ export const AltListItem: React.FC<AltList> = ({ alt, onUseAlt, onRemoveAlt }) =
     const [favorite, setFavorite] = React.useState(false);
     const [dropdownOpen, setDropdownOpen] = React.useState(false);
     const [altHeadshot, setAltHeadshot] = React.useState<string>("");
+    const [deleteStage, setDeleteStage] = React.useState(0);
+    const [altname, setAltname] = React.useState(alt);
+    useEffect(() => {
+        let active = true;
+        load();
+        return () => { active = false };
+        async function load() {
+            if (active) {
+                try {
+                    if (!isNaN(Number(alt))) {
+                        const username = await getRobloxUserById(alt);
+                        if (username.Username)
+                            setAltname(username.Username);
+                        else if (username.username)
+                            setAltname(username.username);
+                    }
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        }
+    }, [alt]);
+
 
     const onClickUseAlt = () => {
         onUseAlt(alt);
@@ -49,6 +72,24 @@ export const AltListItem: React.FC<AltList> = ({ alt, onUseAlt, onRemoveAlt }) =
         }
     }, [alt]);
 
+    const innerOnDelete = () => {
+        setDeleteStage(() => deleteStage + 1);
+        let timeout = setTimeout(() => {
+            setDeleteStage(0);
+            clearTimeout(timeout);
+        }, 1000);
+        console.log(deleteStage)
+        if (deleteStage === 0) {
+            setDeleteStage(1);
+        } else if (deleteStage === 1) {
+            setDeleteStage(2);
+        } else if (deleteStage === 2) {
+            // onDelete()
+        } else {
+            setDeleteStage(0);
+        }
+    }
+
     return (
         <div className="alt-list-item">
             <Container>
@@ -57,23 +98,34 @@ export const AltListItem: React.FC<AltList> = ({ alt, onUseAlt, onRemoveAlt }) =
                         <Avatar size='md' src={headshot} />
                     </Col>
                     <Col>
-                        <Text h3>{alt}</Text>
+                        <Text h3>{altname}</Text>
                     </Col>
                     <Col>
+                        <Spacer y={0.4} />
                         <Button
                             auto
                             type="button"
                             icon={<Star set='curved' filled={favorite} />}
                             onClick={onClickFavorite}
                             color={favorite ? 'warning' : 'primary'}
-                            size='xs'
                             flat
-                         />
-                    </Col>
+                            size='xs'
+                        />
+                        <Spacer y={0.4}/>
+                        <Button
+                            auto
+                            type='button'
+                            icon={<Delete set='curved' filled={deleteStage === 2} />}
+                            onClick={() => innerOnDelete() }
+                            flat
+                            color={deleteStage >= 1 ? 'error' : 'primary'}
+                            size='xs'
+                        />
+                   </Col>
                     <Col>
-                        <Button type='button' onClick={() => onUseAlt(alt)} size='sm'
-                            icon={<Play set='curved' />}
-                            color='success' shadow
+                        <Button type='button' onClick={() => onUseAlt(alt)}
+                            icon={<Play set='curved' />} color='success' shadow
+                            size='sm'
                         >Use</Button> 
                     </Col>
                 </Row>
