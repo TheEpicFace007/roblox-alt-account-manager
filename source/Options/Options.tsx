c2cc00c5eec78f7bad039a8d25c75c2d3ec1b743
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2022-10-19 19:56:39 -0400

    coded options

--- source/Options/Options.tsx
+++ source/Options/Options.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
+import { AltListItem } from '../shared-component/AltListItem';
+import { AltAccount } from '../shared/storage';
 
 import './styles.scss';
 
 const Options: React.FC = () => {
+  const [alts, setAlts] = React.useState<AltAccount[]>([]);
+  React.useEffect(() => {
+    load()
+    async function load() {
+      const alts = await AltAccount.getAll()
+      setAlts(alts)
+    }
+  }, [])
+
+  function removeAlt(username: string) {
+    AltAccount.getWithUsername(username).then(altAccount => {
+      if (altAccount !== null) {
+        AltAccount.remove(altAccount.id)
+        window.location.reload()
+      } 
+    })
+  }
   return (
     <div>
-      
+      <div className="options">
+        {alts.map((alt, i) => (
+          <AltListItem key={i} alt={alt.username} onRemoveAlt={removeAlt} onUseAlt={function () {}}/>
+        ))}
+      </div>
     </div>
   );
 };
